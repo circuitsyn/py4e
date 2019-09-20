e3abcae04b79ac9b3d233050858a1cb2e5c9eec2
CircuitSyn <circuitsyn@gmail.com>
2019-09-19 17:30:03 -0700

    fixed unreachable code, had to move print statement outside of code with break

--- WK5/5.2.py
+++ WK5/5.2.py
@@ -6,21 +6,23 @@ smallest = None
 while True:
     try :
         num = input("Enter a number: ")
+    
+
+        if num == "done" :
+            break
+        else :
+            print(num)
+            if smallest is None :
+                smallest = num
+            if largest is None :
+                largest = num
+            if num > largest :
+                largest = num
+            if num < smallest :
+                smallest = num
+
     except :
         print("Invalid Integer check your input")
-
-    if num == "done" :
-        break
-        print(num)
-    else :
-        if smallest is None :
-            smallest = num
-        if largest is None :
-            largest = num
-        if num > largest :
-            largest = num
-        if num < smallest :
-            smallest = num
 
 print("Minimum", smallest)
 print("Maximum", largest)
