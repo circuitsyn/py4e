bf33b9eb09b5da6f350fdc0f2eb52a2eb9821db9
CircuitSyn <circuitsyn@gmail.com>
2019-10-10 08:49:44 -0700

    finished program by adding sorting algorhithm and printing data

--- WK10/10.2.py
+++ WK10/10.2.py
@@ -4,25 +4,7 @@
 
 # Once you have accumulated the counts for each hour, print out the counts, sorted by hour as shown below.
 
-#desired output
-# 04 3
-# 06 1
-# 07 1
-# 09 2
-# 10 3
-# 11 6
-# 14 1
-# 15 2
-# 16 4
-# 17 2
-# 18 1
-# 19 1
-
-#########################################################
-count = 0
 usrGrp = dict()
-winnerCount = None
-winnerEmail = None
 
 #function to process line
 def processor(line) :
@@ -32,14 +14,12 @@ def processor(line) :
     hrCap = starterTime.split(":")
     #print("hr cap time:",hrCap)
     finishedData = hrCap[0]
-    #print("finished time:",finishedData)
     #code line that creates key and values for our dictionary
     usrGrp[finishedData] = usrGrp.get(finishedData,0) + 1
     return finishedData
 
 fname = input("Enter file name: ")
 if len(fname) < 1 : fname = "mbox-short.txt"
-
 fh = open(fname)
 
 #loop to go through each line of text
@@ -48,13 +28,10 @@ for line in fh:
     if not line.startswith("From ") : continue
     #call function to process text data
     processor(line)
-    print("usr grp", usrGrp)
-    # for email,count in usrGrp.items():
-    #     if winnerCount is None or count > winnerCount:
-    #         winnerEmail = email
-    #         winnerCount = count
-#print(usrGrp)
-for (x,y) in usrGrp :
+
+#Sorts tuple data and stores it in sortedData
+sortedData = ( sorted( [ (x,y) for x,y in usrGrp.items() ] ) )
+
+#runs a loop to print sorted final data
+for x,y in sortedData :
     print(x,y)
-print(winnerEmail,winnerCount)
-#print("There were", count, "lines in the file with From as the first word")
