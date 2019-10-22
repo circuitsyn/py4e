3a0a7aaabba2de79b50376b711ec3c1dc749da4c
CircuitSyn <circuitsyn@gmail.com>
2019-10-21 23:14:09 -0700

    built working findall that builds needed list for project

--- WK11/11.9.py
+++ WK11/11.9.py
@@ -1,24 +1,22 @@
 import re
-
-def processor(line) :
-    collection = line.split()
-    starterTime = collection[5]
-    #print("starter time:",starterTime)
-    hrCap = starterTime.split(":")
-    #print("hr cap time:",hrCap)
-    finishedData = hrCap[0]
-    #code line that creates key and values for our dictionary
-    usrGrp[finishedData] = usrGrp.get(finishedData,0) + 1
-    return finishedData
+found = list()
+totalItems = list()
 
 fname = input("Enter file name: ")
-if len(fname) < 1 : fname = "mbox-short.txt"
+if len(fname) < 1 : 
+    fname = "regex_sum_42.txt"
+if len(fname) == 1 : 
+    fname = "regex_sum_277128.txt"
 fh = open(fname)
 
 #loop to go through each line of text
 for line in fh:
-    #skip lines not starting with desired text
-    #if not line.startswith("From ") : continue
-    #call function to process text data
     line = line.rstrip()
-    processor(line)
+    found = (re.findall('[0-9]+', line))
+    print("found: ", found)
+    if len(found) < 1 : continue
+    totalItems.append(found)
+    
+    
+
+print(totalItems)
