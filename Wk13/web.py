01c13ed4a018e232d22f034a737bb4ca5a17501d
CircuitSyn <circuitsyn@gmail.com>
2019-11-03 18:54:57 -0800

    added int converstion for contents addition

--- Wk13/web.py
+++ Wk13/web.py
@@ -11,15 +11,21 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
+total = 0
+count = 0
 url = input('Enter - ')
 html = urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, "html.parser")
 
 # Retrieve all of the anchor tags
-tags = soup('a')
+tags = soup('span')
 for tag in tags:
+    total = total + int(tag.contents[0])
+    count = count + 1
     # Look at the parts of a tag
     print('TAG:', tag)
     print('URL:', tag.get('href', None))
     print('Contents:', tag.contents[0])
     print('Attrs:', tag.attrs)
+    print("Total:", total)
+    print("Count:", count)
