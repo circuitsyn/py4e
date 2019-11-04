5eafd7af20f655a5dcabb1e7f538cf7f059f93f4
CircuitSyn <circuitsyn@gmail.com>
2019-11-03 21:41:26 -0800

    added nested loop sequence

--- Wk13/webScraperB.py
+++ Wk13/webScraperB.py
@@ -11,8 +11,8 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-total = 0
 count = 0
+complete = 0
 url = input('Enter - ')
 howMany = input('How many times?')
 position = input('Position?')
@@ -23,10 +23,14 @@ soup = BeautifulSoup(html, "html.parser")
 tags = soup('a')
 for tag in tags:
     count = count + 1
-    # Look at the parts of a tag
-    print('TAG:', tag)
-    print('URL:', tag.get('href', None))
-    print('Contents:', tag.contents[0])
-    print('Attrs:', tag.attrs)
-    print("Total:", total)
     print("Count:", count)
+    if(count == position) :
+        print("completed", completed)
+        while(completed < howMany) :
+            completed = completed + 1
+            # Look at the parts of a tag
+            print('TAG:', tag)
+            print('URL:', tag.get('href', None))
+            print('Contents:', tag.contents[0])
+            print('Attrs:', tag.attrs)
+            
