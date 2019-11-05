5f1631aff8c6648cc9726a2b218ad8b8d57f3195
CircuitSyn <circuitsyn@gmail.com>
2019-11-04 18:56:01 -0800

    proper extraction of href

--- Wk13/webScraperB.py
+++ Wk13/webScraperB.py
@@ -6,31 +6,61 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import ssl
 
+tags = list()
+count = 0
+completed = 0
+
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-count = 0
-complete = 0
-url = input('Enter - ')
-howMany = input('How many times?')
-position = input('Position?')
-html = urlopen(url, context=ctx).read()
-soup = BeautifulSoup(html, "html.parser")
+def openUrl(url) :
+    html = urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, "html.parser")
 
-# Retrieve all of the anchor tags
-tags = soup('a')
-for tag in tags:
-    count = count + 1
-    print("Count:", count)
-    if(count == position) :
-        print("completed", completed)
-        while(completed < howMany) :
-            completed = completed + 1
-            # Look at the parts of a tag
-            print('TAG:', tag)
-            print('URL:', tag.get('href', None))
-            print('Contents:', tag.contents[0])
-            print('Attrs:', tag.attrs)
+    # Retrieve all of the anchor tags
+    tags = soup('a')
+    # print('TAGs first:', tags)
+
+    for tag in range(0, position):
+        print("Tag--: ", tags[tag])
+        current = tags[tag]
+        print(current.get('href', None))
+        # print("Tag--: ", tag[tag].get('href', None))
+
+    # scanData(tags)
+    return tags
+
+# def scanData(tags) :
+#     print("in scanndata")
+#     print('TAGs:', tags)
+    
+#     for tag in tags:
+#         count = count + 1
+#         print("Count:", count)
+#         if(count <= position) :
+#             print("completed", completed)
+#             while(completed < howMany) :
+#                 print("completed", completed)
+#                 completed = completed + 1
+#                 # Look at the parts of a tag
+#                 print('TAG:', tag)
+#                 print('URL:', tag.get('href', None))
+#                 print('Contents:', tag.contents[0])
+#                 print('Attrs:', tag.attrs)
+#         else :
+#             quit()
+
+# def printTag(tags) :
+#     print("tags printed spec", tags)
+
+url = input('Enter - ')
+howMany = int(input('How many times? '))
+position = int(input('Position? '))
+
+openUrl(url)
+# print("tags", tags)
+# print('TAGs ouside:', tags)
+
             
