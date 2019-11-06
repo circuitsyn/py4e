fcd299a9a1956b8fd9b13389c6cfbf9bae2e0b0c
CircuitSyn <circuitsyn@gmail.com>
2019-11-05 22:54:34 -0800

    working code, needed brackets instead of dot notation to pull specific child data

--- Wk13/json1.py
+++ Wk13/json1.py
@@ -17,18 +17,8 @@ if (len(url) < 1) :
 uh = urllib.request.urlopen(url, context=ctx)
 data = uh.read()
 
-# Test code output
-# print("Data:", data.decode())
-
 info = json.loads(data)
-print('User Count:', len(info))
 
-# store tree xml data in tree
-# tree = ET.fromstring(data)
-# pull count data from xml
-# counts = tree.findall('.//count')
-
-# for count in counts :
-#     # print("count:", count)
-#     total = total + int(count.text)
-# print("total", total)
+for item in info['comments'] :
+    total = total + int(item['count'])
+print("Total:", total)
