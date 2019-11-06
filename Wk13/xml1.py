ec6d831029d3b5471b68f9c1f54a513ff47fdaa1
CircuitSyn <circuitsyn@gmail.com>
2019-11-05 18:31:41 -0800

    completed withw orking sum calculation

--- Wk13/xml1.py
+++ Wk13/xml1.py
@@ -2,17 +2,6 @@ import urllib.request, urllib.parse, urllib.error
 import xml.etree.ElementTree as ET
 import ssl
 
-# api_key = False
-# If you have a Google Places API key, enter it here
-# api_key = 'AIzaSy___IDByT70'
-# https://developers.google.com/maps/documentation/geocoding/intro
-
-# if api_key is False:
-#     api_key = 42
-#     serviceurl = 'http://py4e-data.dr-chuck.net/xml?'
-# else :
-#     serviceurl = 'https://maps.googleapis.com/maps/api/geocode/xml?'
-
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
@@ -29,36 +18,13 @@ data = uh.read()
 
 # Test code output
 # print("Data:", data.decode())
+
 # store tree xml data in tree
 tree = ET.fromstring(data)
 # pull count data from xml
 counts = tree.findall('.//count')
 
-# for count in counts :
-#     print("count:", count)
-#     total = total + int(count.txt)
-# print("total", total)
-print("counts", counts)
-# while True:
-#     address = input('Enter location: ')
-#     if len(address) < 1: break
-
-#     parms = dict()
-#     parms['address'] = address
-#     if api_key is not False: parms['key'] = api_key
-#     url = serviceurl + urllib.parse.urlencode(parms)
-#     print('Retrieving', url)
-#     uh = urllib.request.urlopen(url, context=ctx)
-
-#     data = uh.read()
-#     print('Retrieved', len(data), 'characters')
-#     print(data.decode())
-#     tree = ET.fromstring(data)
-
-#     results = tree.findall('result')
-#     lat = results[0].find('geometry').find('location').find('lat').text
-#     lng = results[0].find('geometry').find('location').find('lng').text
-#     location = results[0].find('formatted_address').text
-
-#     print('lat', lat, 'lng', lng)
-#     print(location)
+for count in counts :
+    # print("count:", count)
+    total = total + int(count.text)
+print("total", total)
